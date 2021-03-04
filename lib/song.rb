ba28426a544e61b36322f216cdd5b671b2c06424
Jacob W Jones <jacobwadejones76@gmail.com>
2021-03-04 20:14:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
   @@count = 0
+  @@artists = []
+  @@genres = []
   
 def initialize(name, artist, genre)
   @name = name
@@ -10,6 +12,8 @@ def initialize(name, artist, genre)
   @@genres << genre
 
 end
+
+def self.count
 
 
   
